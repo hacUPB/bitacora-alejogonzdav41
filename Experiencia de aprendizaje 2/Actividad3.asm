cc0172375b81dbbe724f165ee8e1e4e553773ced
alejogonzdav41 <alejandro.gonzalezdavid@upb.edu.co>
2025-07-28 17:17:29 -0500

    qefeqfqwqfqff

---
+++ Experiencia de aprendizaje 2/Actividad3.asm
@@ -0,0 +1,3 @@
+// Primero vamos a la posicion 24576 por medio de @KBD o @24576
+// Luego leemos lo que hay en la posicion y le restamos 100
+// Despues comparamos el resultado para saber si se realizara el salto a @DRAW, si se cumple la igualdaad a 0 se procedera a al dibujo, si no segura en el loop.
